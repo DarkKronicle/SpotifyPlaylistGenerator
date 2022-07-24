02bf92b8f664bb7557383d58e04a9b880ba0067c
DarkKronicle <darkkronicle@gmail.com>
2022-07-24 13:31:52 -0600

    Add wheels

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+import pathlib
+
+requirements = [s.strip() for s in pathlib.Path('requirements.txt').read_text().split('\n')]
+print(requirements)
+
+setup(
+    name='spotifygenerator',
+    packages=find_packages(include=['generator']),
+    version='0.1.0',
+    description='A library for generating Spotify playlists',
+    author='DarkKronicle',
+    license='MIT',
+    install_requires=requirements,
+)
